309be1e13c4ff150a471f7c4b5c7639aab02ea4c
oriya <oriyaroth3@gmail.com>
2022-08-15 16:05:54 +0300

    fixing images fade

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -7,7 +7,7 @@ import jewl from "../../assets/carouselimages/jewllery.jpg"
 export function BsCarousel() {
   const navigate = useNavigate()
   return (
-    <Carousel fade>
+    <Carousel>
       <Carousel.Item>
         <img
           className="d-block w-100"
